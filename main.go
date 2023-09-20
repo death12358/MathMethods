1f7967ef0f7c21985ff2d4c279949d7f7abeac2a
Dennis <l10714056@gmail.com>
2023-09-20 15:52:16 +0800

    s

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	float64conversion "github.com/death12358/text-conversion/float64conversion"
 )
 
+func Test() {
+	float64conversion.Decimal([]float64{123})
+}
 func main() {
-
+	Test()
 	fmt.Print("hello world")
-	dot.test()
+
 }
